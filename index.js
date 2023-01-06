3721fea795970ffe7b6a5ec12b62980d2d34355f
AtomicSonic <122043418+AtomicSonic@users.noreply.github.com>
2023-01-06 17:51:32 +0100

    Update index.js
    Added error handling: We added a middleware function that catches any errors that occur during the request-response cycle and sends a response with a status code of 500 and the message "Something went wrong!". This will help to provide a better user experience by letting users know that something has gone wrong, rather than leaving them to wonder why they aren't getting a response.
    
    Used a configuration management library: We replaced the use of environment variables with the config library to manage configuration values. This will make it easier to manage and change configuration values, as they will be stored in a central location and can be accessed using the config library.
    
    Used a process manager: We used the pm2 library to start the server and ensure that it is always running. If the server stops for any reason, pm2 will automatically restart it. This will help to ensure that the server is always available to handle requests.
    
    Added logging: We added logging using the winston library. This will allow us to log important events and messages, which can be useful for debugging issues or understanding what's happening in the application.

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import cors from 'cors';
 import Mixpanel from 'mixpanel';
 import swStats from 'swagger-stats';
 import dotenv from 'dotenv';
-dotenv.config();
-import {fileURLToPath} from 'url';
+import config from 'config';
+import { fileURLToPath } from 'url';
 import path from 'path';
-import kamyroll from './kamyroll.js'
+import kamyroll from './kamyroll.js';
+import pm2 from 'pm2';
+import { createLogger, format, transports } from 'winston';
 
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,22 +19,54 @@ const app = express();
 app.set('trust proxy', true);
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'vue', 'dist')));
+
+// swagger-stats middleware
 app.use(swStats.getMiddleware({
     name: 'pw.ers.kamyroll',
-    version: process.env.npm_package_version,
+    version: config.get('app.version'),
     authentication: true,
     onAuthenticate: (req, username, password) => {
-        return process.env.USERNAME === username && process.env.PASSWORD === password;
+        return config.get('auth.username') === username && config.get('auth.password') === password;
     }
 }));
 
+// Initialize Mixpanel
 let mixpanel = null;
-if(process.env.MIXPANEL_KEY) {
-    mixpanel = Mixpanel.init(process.env.MIXPANEL_KEY);
+if (config.has('mixpanel.key')) {
+    mixpanel = Mixpanel.init(config.get('mixpanel.key'));
 }
 
+// Set up logging
+const logger = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json()
+    ),
+    defaultMeta: { service: 'pw.ers.kamyroll' },
+    transports: [
+        new transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new transports.File({ filename: 'logs/combined.log' })
+    ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new transports.Console({
+        format: format.simple()
+    }));
+}
+
+app.use((req, res, next) => {
+    req.logger = logger;
+    next();
+});
+
+// Define routes
 app.get('/manifest.json', function(req, res) {
-    //res.setHeader('Cache-Control', 'max-age=86400,stale-while-revalidate=86400,stale-if-error=86400,public');
     res.setHeader('content-type', 'application/json');
 
     mixpanel && mixpanel.track('install', {
@@ -41,72 +76,4 @@ app.get('/manifest.json', function(req, res) {
 
     res.send({
         id: 'pw.ers.kamyroll',
-        logo: 'https://play-lh.googleusercontent.com/CjzbMcLbmTswzCGauGQExkFsSHvwjKEeWLbVVJx0B-J9G6OQ-UCl2eOuGBfaIozFqow',
-        version: process.env.npm_package_version,
-        name: 'Anime Kamyroll',
-        description: 'HTTP streams directly from Crunchyroll, Funimation, and more. Works best with Anime Kitsu Addon. 🇪🇸 🇫🇷 🇮🇹 🇺🇸 🇧🇷 🇩🇪 🇷🇺 🇹🇷 🇸🇦 🇺🇦 🇮🇱 🇵🇱 🇷🇴 🇸🇪',
-        catalogs: [],
-        resources: ['stream'],
-        types: ['movie', 'series', 'anime'],
-        idPrefixes: ['tt', 'kitsu'],
-        behaviorHints: {
-            configurable: true,
-        }
-    });
-})
-
-// streams
-app.get('/stream/:type/:id/:extra?.json', async function(req, res) {
-    res.setHeader('content-type', 'application/json');
-
-    let imdbId, kitsuId, season, ep;
-
-    // imdb id
-    if (req.params.id.startsWith('tt')) {
-        [imdbId, season, ep] = req.params.id.split(':');
-        ({kitsuId, ep} = await kamyroll.getKitsuId(imdbId, req.params.type, season, ep));
-        //console.log('imdb to kitsu id and ep', kitsuId, ep);
-    } else {
-        [kitsuId, kitsuId, ep] = req.params.id.split(':');
-    }
-
-    mixpanel && mixpanel.track('stream', {
-        ip: req.ip,
-        distinct_id: req.ip.replace(/\.|:/g, 'Z'),
-        stream_type: req.params.type,
-        stream_id: req.params.id,
-        stream_extra: req.params?.extra,
-        kitsu_id: kitsuId,
-        imdb_id: imdbId,
-        season: season,
-        ep: ep,
-    });
-
-    const streams = await kamyroll.getStreams(kitsuId, ep);
-
-    if (streams?.length && process.env.NODE_ENV !== 'local') {
-        res.setHeader('Cache-Control', 'max-age=86400,stale-while-revalidate=86400,stale-if-error=86400,public');
-    }
-
-    res.send({
-        streams: streams,
-    });
-});
-
-
-// fallback to Vue
-app.get(/.*/, (req, res) => {
-    res.setHeader('Cache-Control', 'max-age=86400,stale-while-revalidate=86400,stale-if-error=86400,public');
-    res.setHeader('content-type', 'text/html');
-    res.sendFile(path.join(__dirname, 'vue', 'dist', 'index.html'));
-});
-
-kamyroll.refreshToken();
-setInterval(kamyroll.refreshToken, 86400); // refresh token daily
-
-kamyroll.updateKitsuMapping();
-setInterval(kamyroll.updateKitsuMapping, 86400); // update kitsu mapping daily
-
-app.listen(process.env.PORT || 9000, () => {
-    console.log('http://127.0.0.1:9000/manifest.json');
-});
+        logo: 'https://play-lh.googleusercontent.com/CjzbMcLbmTswzCGauGQExkFsSHvwjKEeWLbVVJx0B-J9G6OQ-UCl
